48f8aa4dc4822d834e4ee22f8a5930f3079714c0
Rawan Abdallah <ra8905365@gmail.com>
2026-01-24 06:39:16 +0200

    Add project files.

---
+++ Survey_Basket_API/Controllers/PollsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Survey_Basket_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PollsController : ControllerBase
+    {
+    }
+}
